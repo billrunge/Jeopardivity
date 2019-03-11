37e5ff7f7be5126cee566cc25f99782488dfc2c1
billrunge <billrunge@gmail.com>
2019-03-10 19:13:47 -0500

    Beginning to create API for creating a game

---
+++ Jeopardivity.Functions/CreateGame.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Jeopardivity.Functions
+{
+    public static class CreateGame
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return  (ActionResult)new OkObjectResult($"Game code is...");
+        }
+    }
+}
