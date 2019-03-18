bc24c6962e9f1f0c0ec783eb24392c875803dae4
billrunge <billrunge@gmail.com>
2019-03-17 21:59:49 -0500

    Majorly culled down the amount of functions that were needed.

--- Jeopardivity.Functions/GetQuestionStatusFromUser.cs
+++ Jeopardivity.Functions/GetQuestionStatusFromUser.cs
@@ -7,18 +7,12 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Data.SqlClient;
-using System.Data;
 using Jeopardivity.Libraries;
 
 namespace Jeopardivity.Functions
 {
     public static class GetQuestionStatusFromUser
     {
-        private static int question;
-        private static bool answerable;
-        private static bool userBuzzed;
-
         [FunctionName("GetQuestionStatusFromUser")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
