24872011ef03f2d7a2ab5fa8c7104875ecde2b85
billrunge <billrunge@gmail.com>
2019-03-18 19:37:27 -0500

    I was able to eliminate two more APIs through optimizing the creategame API. Updated the javascript to refect this as well.

--- Jeopardivity.Functions/CreateGame.cs
+++ Jeopardivity.Functions/CreateGame.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Jeopardivity.Functions
 {
@@ -14,19 +15,32 @@ namespace Jeopardivity.Functions
     {
         [FunctionName("CreateGame")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
 
             Helper helper = new Helper()
             {
                 SqlConnectionString = Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING")
             };
 
+            string userName = data.UserName;
+
             string gameCode = helper.GenerateGameCode(5);
             int game = await helper.CreateGameAsync(gameCode);
 
-            var returnObject = new { Game = game, GameCode = gameCode };
+            int user = await helper.CreateUserAsync(userName, game, true);
+
+            string jwt = await helper.GenerateJwtAsync(user, 
+                            game, 
+                            userName, 
+                            gameCode, 
+                            true, 
+                            Environment.GetEnvironmentVariable("JWT_KEY"));
+
+            var returnObject = new { JWT = jwt };
 
             return new OkObjectResult(JsonConvert.SerializeObject(returnObject));
         }
--- Jeopardivity.UI/Scripts/creategame.js
+++ Jeopardivity.UI/Scripts/creategame.js
@@ -5,50 +5,26 @@ let baseUrl = "http://localhost:7071";
 
 $(document).ready(function () {
 
-    
+
     $("#SubmitButton").click(function (e) {
 
-        userName = $("#UserName").val();
+        $.ajax({
+            type: "POST",
+            url: baseUrl + "/api/CreateGame",
+            contentType: "application/json; charset=utf-8",
+            data: '{ "UserName":"' + $("#UserName").val() + '"}',
+            dataType: "json",
+            success: function (msg) {
+                jwt = msg.JWT;
+                localStorage.JWT = jwt;
+                $(location).attr('href', './alex.html');
+            },
+            error: function (req, status, error) {
+                $("h1").html('<error-text>Unabled to generate JWT</error-text>');
 
-        $.get(baseUrl + "/api/CreateGame", function (data, status) {
-
-            var obj = jQuery.parseJSON(data);
-            game = obj.Game;
-            gameCode = obj.GameCode;
-            console.log(gameCode);
-
-            $.ajax({
-                type: "POST",
-                url: baseUrl + "/api/CreateUser",
-                contentType: "application/json; charset=utf-8",
-                data: '{"Game":"' + game + '", "Name":"' + userName + '","IsAlex":"' + true + '" }',
-                dataType: "json",
-                success: function (msg) {
-                    user = msg.User;
-
-                    $.ajax({
-                        type: "POST",
-                        url: baseUrl + "/api/CreateJWT",
-                        contentType: "application/json; charset=utf-8",
-                        data: '{"User":"' + user + '", "Game":"' + game + '", "UserName":"' + userName + '", "GameCode":"' + gameCode + '",  "IsAlex":"' + true + '" }',
-                        dataType: "json",
-                        success: function (msg) {
-                            jwt = msg.JWT;
-                            localStorage.JWT = jwt;
-                            $(location).attr('href', './alex.html');
-                        },
-                        error: function (req, status, error) {
-                            $("h1").html('<error-text>Unabled to generate JWT</error-text>');
-
-                        }
-                    });
-                },
-                error: function (req, status, error) {
-                    $("h1").html('<error-text>Something bad has happened.</error-text>');
-
-                }
-            });           
+            }
         });
+
         return false;
 
     });
